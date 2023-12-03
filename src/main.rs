895925470292bef768812a7ff0a828c5d6a14627
yuryion <47436704+yuryion@users.noreply.github.com>
2023-12-02 20:50:08 -0600

    2nd commit, learned Components and Systems

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use bevy::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_systems(Startup, add_people)
+        .add_systems(Update, hello_world)
+        .run();
+}
+
+fn hello_world() {
+    println!("hello world!");
+}
+
+#[derive(Component)]
+struct Person;
+
+#[derive(Component)]
+struct Name(String);
+
+fn add_people(mut commands: Commands) {
+    commands.spawn((Person, Name("Elaina Proctor".to_string()))); //.to_string makes it a String type and not 'static str'
+    commands.spawn((Person, Name("Renzo Hume".to_string())));
+    commands.spawn((Person, Name("Zayna Nieves".to_string())));
 }
